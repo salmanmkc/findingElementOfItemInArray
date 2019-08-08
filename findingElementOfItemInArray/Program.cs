caaa225746c99425114533c34165968328762eeb
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-08-08 15:46:38 +0100

    fake loading time to make it seem like the program is doing something, as otherwise it will just do it 'instantly'

--- findingElementOfItemInArray/Program.cs
+++ findingElementOfItemInArray/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace findingElementOfItemInArray
@@ -19,6 +20,15 @@ namespace findingElementOfItemInArray
                 numbers[i] = rnd.Next(1, 100);
             }
             int chosen = rnd.Next(0, j - 1);
+            Console.WriteLine($"{chosen} index has value {numbers[chosen]}");
+
+            //doing this to just make it seem like it takes time to find the number
+            string finding = "finding...";
+            foreach(var c in finding)
+            {
+                Console.Write(c);
+                Thread.Sleep(100);
+            }
 
 
         }
