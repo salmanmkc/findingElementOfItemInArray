e928ae839e655f3f9df2472902f5d6b58060d3d8
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-08-08 15:43:02 +0100

    create a random variable which can be used, then generate numbers to go into the array

--- findingElementOfItemInArray/Program.cs
+++ findingElementOfItemInArray/Program.cs
@@ -11,6 +11,15 @@ namespace findingElementOfItemInArray
         static void Main(string[] args)
         {
             Console.WriteLine("This program will do the array stuff for you");
+            Random rnd = new Random();
+            int j = 10;
+            int[] numbers = new int[j];
+            for(int i = 0; i < j; i++)
+            {
+                numbers[i] = rnd.Next(1, 100);
+            }
+
+
         }
     }
 }
