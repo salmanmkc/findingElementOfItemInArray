41adf776f1da0ac6134f17940eeae54fda217746
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-08-08 16:06:15 +0100

    reverted so I can show the step where each number is randomyl generated

--- findingElementOfItemInArray/Program.cs
+++ findingElementOfItemInArray/Program.cs
@@ -14,69 +14,27 @@ namespace findingElementOfItemInArray
             Console.WriteLine("This program will do the array stuff for you");
             Random rnd = new Random();
             int j = 10;
-            int[] numbers = createRandomArray(rnd, j);
-
-            Console.WriteLine("Your radomly generated array: ");
-            printArray(numbers);
-            int chosen = printChosenValueAndIndex(rnd, j, numbers);
+            int[] numbers = new int[j];
+            for (int i = 0; i < j; i++)
+            {
+                numbers[i] = rnd.Next(1, 100);
+            }
+            int chosen = rnd.Next(0, j - 1);
+            Console.WriteLine($"{chosen} index has value {numbers[chosen]}");
 
             //doing this to just make it seem like it takes time to find the number
-            fakeLoading();
-            Console.WriteLine();
-            findIndex(numbers, chosen);
-            Console.ReadKey();
-
-
-
-        }
-
-        private static void findIndex(int[] numbers, int chosen)
-        {
-            Console.WriteLine($"{numbers[chosen]} has index of {Array.IndexOf(numbers, numbers[chosen])}");
-        }
-
-        private static void fakeLoading()
-        {
             string finding = "finding...";
             foreach (var c in finding)
             {
                 Console.Write(c);
-                Thread.Sleep(200);
-            }
-        }
-
-        private static int printChosenValueAndIndex(Random rnd, int j, int[] numbers)
-        {
-            int chosen = rnd.Next(0, j - 1);
-            Console.WriteLine($"{chosen} index has value {numbers[chosen]}");
-            return chosen;
-        }
-
-        private static void printArray(int[] numbers)
-        {
-            foreach (int i in numbers)
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine();
-        }
-
-        private static int[] createRandomArray(Random rnd, int j)
-        {
-            int[] numbers = new int[j];
-            for (int i = 0; i < j; i++)
-            {
-                //need to make sure there are no number repeats
-                int randomly = rnd.Next(1, 11);
-                while (numbers.Contains(randomly))
-                {
-                    randomly = rnd.Next(1, 11);
-
-                }
-                numbers[i] = randomly;
+                Thread.Sleep(100);
             }
 
-            return numbers;
+            Console.WriteLine($"{numbers[chosen]} has index of {Array.IndexOf(numbers, numbers[chosen])}");
+            Console.ReadKey();
+
+
+
         }
     }
 }
